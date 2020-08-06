6c99250e46f0ddf56235c5a7d5eb8ca2b8b0acb2
yslim83 <58931496+yslim83@users.noreply.github.com>
2020-08-06 13:17:59 +0900

    Update PolicyHandler.java

--- src/main/java/maskShop3/PolicyHandler.java
+++ src/main/java/maskShop3/PolicyHandler.java
@@ -9,27 +9,26 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PolicyHandler{
+public class PolicyHandler {
 
     @Autowired
     InventoryRepository inventoryRepository;
 
     @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverDeliveryRegisterd_Change(@Payload DeliveryRegisterd deliveryRegisterd){
+    public void wheneverDeliveryRegisterd_Change(@Payload DeliveryRegisterd deliveryRegisterd) {
 
-        if(deliveryRegisterd.isMe()){
+        if (deliveryRegisterd.isMe()) {
             System.out.println("##### listener REGISTER Change : " + deliveryRegisterd.toJson());
 
             Inventory inventory = new Inventory();
 
-            if(inventoryRepository.findByProductId(deliveryRegisterd.getProductId()).isPresent()) {
+            if (inventoryRepository.findByProductId(deliveryRegisterd.getProductId()).isPresent()) {
                 System.out.println("##### listener INVENTORY UPDATE ======================");
 
                 inventory = inventoryRepository.findByProductId(deliveryRegisterd.getProductId()).get();
                 inventory.setInvQty(inventory.getInvQty() - deliveryRegisterd.getInvQty());
                 inventoryRepository.save(inventory);
-            }
-             else {
+            } else {
                 System.out.println("##### listener INVENTORY INSERT ======================");
 
                 inventory.setProductId(deliveryRegisterd.getProductId());
@@ -40,10 +39,10 @@ public class PolicyHandler{
     }
 
     @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverDeliveryCanceled_Change(@Payload DeliveryCanceled deliveryCanceled){
+    public void wheneverDeliveryCanceled_Change(@Payload DeliveryCanceled deliveryCanceled) {
 
-        if(deliveryCanceled.isMe()){
-            System.out.println("##### listener INVENTORY CANCEL IF : "+ deliveryCanceled.toJson());
+        if (deliveryCanceled.isMe()) {
+            System.out.println("##### listener INVENTORY CANCEL IF : " + deliveryCanceled.toJson());
 
             Inventory inventory = new Inventory();
 
@@ -52,6 +51,7 @@ public class PolicyHandler{
             inventoryRepository.save(inventory);
         }
     }
+
     //0805 lys수정 start
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverProductModified_Change(@Payload ProductModified productModified) {
@@ -74,5 +74,3 @@ public class PolicyHandler{
     }
 }
 //0805 lys수정 end
-
-}
